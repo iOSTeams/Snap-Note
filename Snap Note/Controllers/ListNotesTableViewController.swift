5a01da6d7b0d4fb6484c9e1ee91b950c78a430db
Justin Lee <jlee402@mail.ccsf.edu>
2016-07-07 14:29:07 -0700

    Snap format

--- Snap Note/Controllers/ListNotesTableViewController.swift
+++ Snap Note/Controllers/ListNotesTableViewController.swift
@@ -34,18 +34,17 @@ class ListNotesTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 10
     }
-
-    /*
+    
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        
+        cell.textLabel?.text = "list2"
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
--- Snap Note/ViewController.swift
+++ Snap Note/ViewController.swift
@@ -13,39 +13,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        //
-        
-        var cameraView : CameraViewController = CameraViewController(nibName: "CameraViewController", bundle: nil)
-        
-        var listNotesTableView : ListNotesTableViewController = ListNotesTableViewController(nibName: "ListNotesTableViewController", bundle: nil)
-        
 
+        view.backgroundColor = .greenColor()
         
-        var listTwo : ListTwoTableViewController = ListTwoTableViewController(nibName: "ListTwoTableViewController", bundle: nil)
+        let cameraView : CameraViewController = CameraViewController(nibName: "CameraViewController", bundle: nil)
         
+        let one : ListNotesTableViewController = ListNotesTableViewController(nibName: "ListNotesTableViewController", bundle: nil)
+        
+        let two : ListTwoTableViewController = ListTwoTableViewController(nibName: "ListTwoTableViewController", bundle: nil)
+//        let two = self.storyboard?.instantiateViewControllerWithIdentifier("ListTwoTableViewController") as! ListTwoTableViewController
+        
+        self.scrollView.bounces = false
         
         
         self.addChildViewController(cameraView)
-    self.scrollView.addSubview(cameraView.view)
+        self.scrollView.addSubview(cameraView.view)
         cameraView.didMoveToParentViewController(self)
         
+        self.addChildViewController(one)
+        self.scrollView.addSubview(one.view)
+        one.didMoveToParentViewController(self)
         
-        self.addChildViewController(listNotesTableView)
-        self.scrollView.addSubview(listNotesTableView.view)
-        listNotesTableView.didMoveToParentViewController(self)
-
-        self.addChildViewController(listTwo)
-        self.scrollView.addSubview(listTwo.view)
-        listTwo.didMoveToParentViewController(self)
+        self.addChildViewController(two)
+        self.scrollView.addSubview(two.view)
+        two.didMoveToParentViewController(self)
         
         var cameraFrame : CGRect = cameraView.view.frame
         cameraFrame.origin.x = self.view.frame.width
         cameraView.view.frame = cameraFrame
         
+        two.view.frame = CGRectMake(2*self.view.frame.width, 0, two.view.frame.size.width, two.view.frame.size.height)
+        
         self.scrollView.contentSize = CGSizeMake(self.view.frame.width * 3, self.view.frame.size.height)
-        self.scrollView.contentOffset = CGPoint(x: cameraFrame.origin.x, y: cameraFrame.origin.y)
+//        self.scrollView.contentOffset = CGPoint(x: cameraFrame.origin.x, y: cameraFrame.origin.y)
     }
     
         
